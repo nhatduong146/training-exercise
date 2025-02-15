b65cea3777f59f6d4dd290fa2bbc4b5d06456071
nhatduong146 <nhatduong146@gmail.com>
2025-02-15 20:36:36 +0700

    create advanced analyzers project by Roslyn APIs

---
+++ AdvancedAnalyzer/AdvancedAnalyzer/MakeConstAnalyzer.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace AdvancedAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class MakeConstAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "MakeConstAnalyzer";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.MakeConstAnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.MakeConstAnalyzerFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.MakeConstAnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Usage";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticId, 
+            Title, 
+            MessageFormat, 
+            Category, DiagnosticSeverity.Warning, 
+            isEnabledByDefault: true, 
+            description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics 
+        { 
+            get { return ImmutableArray.Create(Rule); } 
+        }
+
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.LocalDeclarationStatement);
+        }
+
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var localDeclaration = (LocalDeclarationStatementSyntax)context.Node;
+
+            // Make sure the declaration isn't already const:
+            if (localDeclaration.Modifiers.Any(SyntaxKind.ConstKeyword))
+            {
+                return;
+            }
+
+            // Perform data flow analysis on the local declaration.
+            DataFlowAnalysis dataFlowAnalysis = context.SemanticModel.AnalyzeDataFlow(localDeclaration);
+
+            // Retrieve the local symbol for each variable in the local declaration
+            // and ensure that it is not written outside of the data flow analysis region.
+            VariableDeclaratorSyntax variable = localDeclaration.Declaration.Variables.Single();
+            ISymbol variableSymbol = context.SemanticModel.GetDeclaredSymbol(variable, context.CancellationToken);
+            if (dataFlowAnalysis.WrittenOutside.Contains(variableSymbol))
+            {
+                return;
+            }
+
+            context.ReportDiagnostic(Diagnostic.Create(
+                Rule, 
+                context.Node.GetLocation(), 
+                localDeclaration.Declaration.Variables.First().Identifier.ValueText));
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+The collection of practical repositories for learning purposes
